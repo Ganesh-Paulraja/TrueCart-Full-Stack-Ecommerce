c600b10dc8713853d53f42ae59bb56818b4ee02d
GANESH_PAULRAJA <ganeshsgk555@gmail.com>
2025-08-23 12:49:52 +0530

    update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ import HandleError from "../utils/handleError.js"
 import handleAsyncError from "../middleware/handleAsyncError.js"
 
 //Creating Products
-export const createProducts =  handleAsyncError(async (req, res, next) => {
+export const createProducts = handleAsyncError(async (req, res, next) => {
     const product = await productModel.create(req.body)
     res.status(200).json({
         success: true,
@@ -64,8 +64,9 @@ export const deleteProdut = handleAsyncError(async (req, res) => {
             message: 'Product not found'
         })
     }
-    res.status(200).json({
+    res.status(200).json({ 
         success: true,
         message: "Product Deleted Successfully"
     }) 
 })
+
